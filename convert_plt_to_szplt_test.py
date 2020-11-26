5a5e1270fc00ef825203fbad6395cc886b67d5ed
Florent Pled <florent.pled@u-pem.fr>
2020-11-26 09:07:24 +0100

    Update convert_plt_to_szplt_test.py

--- convert_plt_to_szplt_test.py
+++ convert_plt_to_szplt_test.py
@@ -4,9 +4,9 @@ import os
 
 for g in [32]:
     print "grid",g
-    for s in range(2,4):
+    for s in [2,3,4]:
         print "std",s
-        for l in range(1,6):
+        for l in range(1,7):
             print "sample",l
             for t in range(0,20):
                 os.system('tec360 -convert ./Grid'+str(g)+'/EC010-'+str(s)+'/Set'+str(l)+'/PhaseInv3D_ramdom_i0000'+str(t)+'00.plt -o ./Grid'+str(g)+'/EC010-'+str(s)+'/Set'+str(l)+'/PhaseInv3D_random_i0000'+str(t)+'00.szplt')
