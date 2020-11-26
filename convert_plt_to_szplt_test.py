e0ec4cf998bba683db6772bbf66568e07e178580
Florent Pled <florent.pled@u-pem.fr>
2020-11-26 10:12:13 +0100

    Update convert plt and read szplt for test

--- convert_plt_to_szplt_test.py
+++ convert_plt_to_szplt_test.py
@@ -8,8 +8,10 @@ for g in [32]:
         print "std",s
         for l in range(1,7):
             print "sample",l
-            for t in range(0,20):
-                os.system('tec360 -convert ./Grid'+str(g)+'/EC010-'+str(s)+'/Set'+str(l)+'/PhaseInv3D_ramdom_i0000'+str(t)+'00.plt -o ./Grid'+str(g)+'/EC010-'+str(s)+'/Set'+str(l)+'/PhaseInv3D_random_i0000'+str(t)+'00.szplt')
+            for t in range(0,10):
+                os.system('tec360 -convert ./Grid'+str(g)+'/EC010-'+str(s)+'/Set'+str(l)+'/PhaseInv3D_ramdom_i00000'+str(t)+'00.plt -o ./Grid'+str(g)+'/EC010-'+str(s)+'/Set'+str(l)+'/PhaseInv3D_ramdom_i00000'+str(t)+'00.szplt')
+            for t in range(10,21):
+                os.system('tec360 -convert ./Grid'+str(g)+'/EC010-'+str(s)+'/Set'+str(l)+'/PhaseInv3D_ramdom_i0000'+str(t)+'00.plt -o ./Grid'+str(g)+'/EC010-'+str(s)+'/Set'+str(l)+'/PhaseInv3D_ramdom_i0000'+str(t)+'00.szplt')
 
 
 
---
+++ read_tecio_szplt_test.m
@@ -0,0 +1,125 @@
+clc
+clearvars
+close all
+% rng('default');
+myparallel('start');
+
+%%%%%%%%%% TecIO Setup %%%%%%%%%%%%%
+if ismac
+    tecplot_home = '/Applications/Tecplot 360 EX 2018 R2/Tecplot 360 EX 2018 R2.app/Contents/MacOS';
+    tecio_path = strcat(tecplot_home,'/libtecio.dylib');
+elseif isunix
+    % tecplot_home = '/usr/local/apps/tecplot360';
+    % tecio_path = strcat(tecplot_home,'/lib/libtec.so');
+    tecplot_home = '/home/p/pled/Applications/Tecplot/360ex_2018r2';
+    tecio_path = strcat(tecplot_home,'/bin/libtecio.so');
+end
+tecio_header_path = 'TECIO.h'; % uses simplified TECIO.h included in package. 
+if ~libisloaded('tecio')
+    [notfound,warnings]=loadlibrary(tecio_path,tecio_header_path,'alias','tecio');
+end
+% libfunctionsview('tecio')
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+varnames = {'U','V','W','sca01','sca02','sca04'}; % variable names
+n = length(varnames); % number of variables
+p = 20; % number of time steps
+N = 6; % number of samples
+
+% pathname = fileparts(mfilename('fullpath'));
+pathname = '/mnt/tcm13/SV_FP/';
+
+for g=2^5
+    tic
+    gridname = ['Grid' num2str(g)];
+    disp(gridname)
+    
+    m = (g+1)^3; % number of spatial points
+    for s=2:4
+    % Y = zeros(N,m*n,p+1);
+    Y = zeros(N,n,m,p+1);
+    for l=1:N
+        foldername = fullfile(['EC010-' num2str(s)],['Set' num2str(l)]);
+        disp(foldername)
+        
+        % Yl = zeros(m*n,p+1);
+        Yl = zeros(n,m,p+1);
+        for t=0:p
+            time = ['Time ' num2str(t)];
+            disp(time)
+            
+            filename = ['PhaseInv3D_ramdom_i' num2str(t,'%06d00') '.szplt'];
+            
+            file = fullfile(pathname,gridname,foldername,filename);
+            if ~exist(file)
+                break
+            end
+            [isok,~,handle] = calllib('tecio','tecFileReaderOpen',file,[]);
+            
+            title = libpointer('stringPtrPtr',cell(1,1));
+            [isok,~,title] = calllib('tecio','tecDataSetGetTitle',handle,title);
+            
+            numvars = 0;
+            numzones = 0;
+            [isok,~,numvars] = calllib('tecio','tecDataSetGetNumVars',handle,numvars);
+            [isok,~,numzones] = calllib('tecio','tecDataSetGetNumZones',handle,numzones);
+            if numvars<n
+                error(['Wrong number of variables in file ' file])
+            end
+            if numzones>1
+                error(['Wrong number of zones in file ' file])
+            end
+            
+            % Yt = zeros(m*n,1);
+            Yt = zeros(n,m);
+            for var=1:numvars
+                name = libpointer('stringPtrPtr',cell(1,1));
+                [isok,~,name] = calllib('tecio','tecVarGetName',handle,var,name);
+                [isvar,i] = ismember(name,varnames);
+                if isvar
+                    zone = 1;
+                    % for zone=1:numzones
+%                         type = 0;
+%                         [isok,~,type] = calllib('tecio','tecZoneGetType',handle,zone,type);
+%                         
+%                         % 0 - Ordered, 1- FE line, ... 5- FE Brick...
+%                         if type == 0
+%                             I = 0; J = 0; K = 0;
+%                             [isok,~,I,J,K] = calllib('tecio','tecZoneGetIJK',handle,zone,I,J,K);
+%                         end
+                        
+                        numvals = 0;
+                        [isok,~,numvals] = calllib('tecio','tecZoneVarGetNumValues',handle,zone,var,numvals);
+                        if numvals~=m
+                            error(['Wrong dataset for variable ' name{:} ' in file ' file])
+                        end
+                        
+                        values = zeros(numvals,1);
+                        [isok,~,values] = calllib('tecio','tecZoneVarGetFloatValues',handle,zone,var,1,numvals,values);
+                        
+                        % Yt((0:m-1)*n+i) = values;
+                        Yt(i,:) = values;
+                    % end
+                end
+            end
+            % Yl(:,t+1) = Yt;
+            Yl(:,:,t+1) = Yt;
+            
+            calllib('tecio','tecFileReaderClose',handle);
+        end
+        % Y(l,:,:) = Yl;
+        Y(l,:,:,:) = Yl;
+        
+    end
+    fprintf('\n');
+    save(fullfile(pathname,gridname,['data_' num2str(s) '.mat']),'Y','N','n','m','p');
+    end
+    toc
+end
+
+if libisloaded('tecio')
+    unloadlibrary('tecio')
+end
+
+myparallel('stop');
