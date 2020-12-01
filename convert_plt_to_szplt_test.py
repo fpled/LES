ad637384537604a5d7e0aeba145a9df54827c8d7
Florent Pled <florent.pled@u-pem.fr>
2020-12-01 17:16:25 +0100

    Update convert_plt_to_szplt_test.py

--- convert_plt_to_szplt_test.py
+++ convert_plt_to_szplt_test.py
@@ -4,14 +4,14 @@ import os
 
 for g in [32]:
     print "grid",g
-    for s in [2,3,4]:
-        print "std",s
+    for s in ["10-1","10-2","510-1"]:
+        print "std=",s
         for l in range(1,7):
             print "sample",l
             for t in range(0,10):
-                os.system('tec360 -convert ./Grid'+str(g)+'/EC010-'+str(s)+'/Set'+str(l)+'/PhaseInv3D_ramdom_i00000'+str(t)+'00.plt -o ./Grid'+str(g)+'/EC010-'+str(s)+'/Set'+str(l)+'/PhaseInv3D_ramdom_i00000'+str(t)+'00.szplt')
+                os.system('tec360 -convert ./Grid'+str(g)+'/EC0'+s+'/Set'+str(l)+'/PhaseInv3D_ramdom_i00000'+str(t)+'00.plt -o ./Grid'+str(g)+'/EC010-'+str(s)+'/Set'+str(l)+'/PhaseInv3D_ramdom_i00000'+str(t)+'00.szplt')
             for t in range(10,21):
-                os.system('tec360 -convert ./Grid'+str(g)+'/EC010-'+str(s)+'/Set'+str(l)+'/PhaseInv3D_ramdom_i0000'+str(t)+'00.plt -o ./Grid'+str(g)+'/EC010-'+str(s)+'/Set'+str(l)+'/PhaseInv3D_ramdom_i0000'+str(t)+'00.szplt')
+                os.system('tec360 -convert ./Grid'+str(g)+'/EC0'+s+'/Set'+str(l)+'/PhaseInv3D_ramdom_i0000'+str(t)+'00.plt -o ./Grid'+str(g)+'/EC010-'+str(s)+'/Set'+str(l)+'/PhaseInv3D_ramdom_i0000'+str(t)+'00.szplt')
 
 
 
