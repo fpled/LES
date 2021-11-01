95e8f519e4357aaf1ab29c43d2a0879eca4ce18c
Florent Pled <florent.pled@icloud.com>
2021-11-01 20:30:25 +0100

    Update laplacian.m

--- laplacian.m
+++ laplacian.m
@@ -6,7 +6,7 @@ for i=1:3
     order = [i+1,setdiff(1:ndims(u),i+1)];
     ui = permute(u,order);
     si = size(ui);
-    laplacianui = reshape(Dxx*ui(:,:),si);
+    laplacianui = reshape(Dxx{i}*ui(:,:),si);
     laplacianui = ipermute(laplacianui,order);
     laplacianu(:,:) = laplacianu(:,:) + laplacianui(:,:);
 end
